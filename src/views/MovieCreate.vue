3772776b289c907c4c3ea9893bd071a706f05eb5
Gail Eddy <gaileddy68@gmail.com>
2021-11-18 10:50:28 -0600

    adds html to create and edit vues

---
+++ src/views/MovieCreate.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="movies-new">
+    <form v-on:submit.prevent="createMovie()">
+      <h1>New movie!</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Title:</label>
+        <input type="text" v-model="newMovieParams.title" />
+      </div>
+      <div>
+        <label>Year:</label>
+        <input type="integer" v-model="newMovieParams.year" />
+      </div>
+      <div>
+        <label>Plot:</label>
+        <input type="text" v-model="newMovieParams.plot" />
+      </div>
+      <div>
+        <label>Director:</label>
+        <input type="text" v-model="newMovieParams.director" />
+      </div>
+      <div>
+        <label>English:</label>
+        <input type="text" v-model="newMovieParams.english" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      newMovieParams: {},
+      errors: [],
+      status: "",
+    };
+  },
+  methods: {
+    createMovie: function () {
+      console.log("Enter a new movie!");
+      axios
+        .post("/movies", this.newMovieParams)
+        .then(() => {
+          this.$router.push("/movies");
+        })
+        .catch((error) => {
+          this.status = error.response.status;
+          console.log(error.response);
+        });
+    },
+  },
+};
+</script>
---
+++ src/views/MovieEdit.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="movies-new">
+    <form v-on:submit.prevent="updateMovie()">
+      <h1>Edit movie!</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Title:</label>
+        <input type="text" v-model="currentMovieParams.title" />
+      </div>
+      <div>
+        <label>Year:</label>
+        <input type="integer" v-model="currentMovieParams.year" />
+      </div>
+      <div>
+        <label>Plot:</label>
+        <input type="text" v-model="currentMovieParams.plot" />
+      </div>
+      <div>
+        <label>Director:</label>
+        <input type="text" v-model="currentMovieParams.director" />
+      </div>
+      <div>
+        <label>English:</label>
+        <input type="text" v-model="currentMovieParams.english" />
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+    <button v-on:click="destroyMovie()">Delete</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      currentmovieParams: {},
+      errors: [],
+    };
+  },
+  created: function () {
+    axios.get(`/movies/${this.$route.params.id}`).then((response) => {
+      console.log("movie Info:", response.data);
+      this.currentMovieParams = response.data;
+    });
+  },
+  methods: {
+    updateMovie: function () {
+      console.log("Fixing that movie!");
+      axios
+        .patch(`/movies/${this.$route.params.id}`, this.currentMovieParams)
+        .then((response) => {
+          this.$router.push(`/movies/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    destroyMovie: function () {
+      axios.delete(`/movies/${this.$route.params.id}`).then((response) => {
+        console.log("Good job!", response.data);
+        this.$router.push("/movies");
+      });
+    },
+  },
+};
+</script>
